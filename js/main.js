54d0d7e93a5567826d931fcaec80618f297f1989
helis.inexture <helis.inexture@gmail.com>
2023-01-12 17:55:37 +0530

    verify chain id before moving further in the game

--- js/main.js
+++ js/main.js
@@ -6,18 +6,37 @@ let gameState = null
 let counter = null
 let owner = null
 
-$(document).ready(function () {
-    fetch("../contract_abi.json").then(
-        response => {
-            return response.json()
-        }
-    ).then(async abi => {
-        contract = new web3.eth.Contract(abi, contractAddress);
-        account = await web3.eth.getAccounts()
-        owner = await contract.methods.owner().call()
-        counter = await contract.methods.counter().call()
-        gameState = await contract.methods.game_state().call()
-    })
+$(document).ready(async function () {
+    const chainId = await web3.eth.getChainId()
+
+    if (chainId === 5) {
+        fetch("../contract_abi.json").then(
+            response => {
+                return response.json()
+            }
+        ).then(async abi => {
+            contract = new web3.eth.Contract(abi, contractAddress);
+            account = await web3.eth.getAccounts()
+            owner = await contract.methods.owner().call()
+            counter = await contract.methods.counter().call()
+            gameState = await contract.methods.game_state().call()
+        })
+    } else {
+        Swal.fire({
+            title: 'Unable to Connect',
+            text: 'You need to connect your MetaMask wallet to Goerli chain to play this game.',
+            icon: 'error',
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            allowEnterKey: false,
+            iconColor: 'beige',
+            customClass: 'swal-style'
+        }).then(() => {
+            setTimeout(() => {
+                window.location.reload()
+            }, 2000)
+        })
+    }
 })
 
 window.ethereum.on('accountsChanged', async function () {
